7aa1f2ced4b229cc0674ce78caffc156b3dbbac6
Livio Alvarenga <livioalvarenga@gmail.com>
2021-10-27 13:11:45 -0300

    add hotreload com arquivo.py

---
+++ hotreload/hotreload_full/main.py
@@ -0,0 +1,33 @@
+import os
+from kivy.core.window import Window
+from kaki.app import App
+from kivymd.app import MDApp
+from manager_screens import ManagerScreens
+
+
+class Live(App, MDApp):
+    KV_FILES = {
+        os.path.join(os.getcwd(), 'Tutoriais_Kivy_KivyMD',
+                     'hotreload', 'hotreload_full', 'manager_screens.kv'),
+        os.path.join(os.getcwd(), 'Tutoriais_Kivy_KivyMD',
+                     'hotreload', 'hotreload_full', 'teste.kv'),
+    }
+
+    CLASSES = {
+        "LoginScreen": 'teste',
+    }
+    AUTORELOADER_PATHS = [(os.getcwd(), {"recursive": True})]
+
+    DEBUG = 1
+
+    def build_app(self):
+        Window.bind(on_keyboard=self._rebuild)
+        self.manager_screens = ManagerScreens()
+        return self.manager_screens
+
+    def _rebuild(self, *args):
+        if args[1] == 32:
+            self.rebuild()
+
+
+Live().run()
